a57510495be82f0d1459c223229046176524efab
AhmedMSallam1 <AhmedM.SallamIbrahim@Gmail.com>
2022-11-27 23:46:54 +0200

    First Push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+/*
+ * Ahmed Mohamed Sallam 20210614
+ * Mohamed Gamal Abdelaziz 20201146
+ * Omar Walid Ahmed 20201126
+ * Ahmed Adel Ali 20201009
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        // Object of a class that has both produce()
+        // and consume() methods
+        GUI gui = new GUI();
     }
+    // This class has a list, producer (adds items to list
+    // and consumer (removes items).
 }
+
+// n=1000000 buffersize=8 outputfile=test.txt
+// n=999999 buffersize=8 outputfile=test.txt
+// n=596374 buffersize=8 outputfile=test.txt
+// n=159753 buffersize=8 outputfile=test.txt
+// n=357951 buffersize=8 outputfile=test.txt
